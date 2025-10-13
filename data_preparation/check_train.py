e16622f5a1f5890ab5745ecac72027b1bf4fd9c5
mohsin32525 <bhatjones@gmail.com>
2025-10-13 20:04:03 +0000

    Add data preparation folder with TSVs and scripts

--- data_preparation/check_train.py
+++ data_preparation/check_train.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 # Load training set
-train_file = "data/train_set.tsv"  # correct folder path
+train_file = "data/train_set.tsv"
 train_df = pd.read_csv(train_file, sep='\t')
 
 # Check the first few rows
 print(train_df.head())
+
